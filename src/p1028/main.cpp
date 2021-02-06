3cb9d964ae21c37d5b6cc78bcaadff7d7d38016c
finlu <18370698988@163.com>
2021-02-06 10:32:51 +0800

    update

--- src/p1028/main.cpp
+++ src/p1028/main.cpp
@@ -3,13 +3,31 @@
 using namespace std;
 
 
+class Person {
+public:
+	string name;
+	int year;
+	int month;
+	int day;
+
+	Person(string name, int year, int month, int day) {
+		this->name = name;
+		this->year = year;
+		this->month = month;
+		this->day = day;
+	}
+}
+
 int main() {
 	int n, count;
-	string maxName, minName, name, maxDate, minDate, date;
+	Person maxAgePerson, minAgePerson, person;
+	string cname;
+	int cyear, cmonth, cday;
+	char c = '/';
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> name >> date;
-		
+		cin >> cname >> cyear >> c >> cmonth >> cday;
+		person = Person(cname, cyear, cmonth, cday);
 	}
 	return 0;
 }
