6bd6d3174d3b5ad221caefb2f2ff67fce03b74e7
finlu <18370698988@163.com>
2021-02-17 20:44:54 +0800

    slove problem 40

---
+++ src/p1040/main.cpp
@@ -0,0 +1,24 @@
+// PAT(Basic Level) Practice
+// https://pintia.cn/problem-sets/994805260223102976/problems/994805282389999616
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int len = s.length(), result = 0, countp = 0, countt = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == 'T') countt++;
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == 'P') countp++;
+        if (s[i] == 'T') countt--;
+        if (s[i] == 'A') result = (result + (countp * countt) % 1000000007) % 1000000007;
+    }
+    cout << result << endl;
+    return 0;
+}
